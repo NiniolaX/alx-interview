e7abcb2c4d1d1d8edc18c2c126c6ed0b153cdff3
NiniolaX <loveafinni16@gmail.com>
2024-08-11 08:17:21 +0100

    Implemented algorithm to unlock locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Lockboxes Algorithm
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Function determines if all boxes in a given list can be ultimately opened.
+    Args:
+        boxes(list of lists): Boxes
+    Return:
+        (bool): True if all boxes can be opened, otherwise, False
+    """
+    # DEPTH-FIRST SEARCH
+    visited = set()
+    to_visit = [0]  # Stack: visit first box first since its unlocked
+
+    while to_visit:
+        current_index = to_visit.pop()
+        if current_index in visited:
+            continue
+        visited.add(current_index)
+        to_visit.extend(boxes[current_index])  # Add keys in this box to stack
+
+    return len(visited) == len(boxes)
+
+#    BREADTH-FIRST SEARCH
+#    from collections import deque
+#
+#    visited = set()
+#    to_visit = deque([0])  # Queue: visit first box first since its unlocked
+#
+#    while to_visit:
+#        current_index = to_visit.popleft()
+#        if current_index in visited:
+#            continue
+#        visited.add(current_index)
+#        to_visit.extend(boxes[current_index])  # add keys here to queue
+#
+#        """For visual display"""
+#        print(f"Just visited box[{current_index}]")
+#        print(f"Total visited {visited}")
+#        print(f"To visit: {to_visit}")
+#        print()
+#
+#    return len(visited) == len(boxes)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,6 @@
+#0x01 - LOCKBOXES
+This repository contains a module which checks if a set of locked boxes with each box containing keys can be unlocked.
+Key concepts here:
+- Algorithms (Breadth-First Search, Depth-First Search)
+- Data Structures (Sets, Stack, Queue)
+- Language (Python)
