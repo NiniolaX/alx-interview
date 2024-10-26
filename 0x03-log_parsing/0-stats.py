071930d0c07d385853d27e9c1ed4afdfcc6be9e6
NiniolaX <loveafinni16@gmail.com>
2024-10-26 20:03:03 +0100

    Modified codebase to skip lines with incorrect file size or status code format

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 """
-This script reads stdin line by line and compute metrics based on read.
+This script reads stdin line by line and displays statistics based on
+accumulated metrics.
 
 Functions:
     print_stats: Prints metrics from stdin read session
     interrupt_handler: Prints metrics upon receival of SIGINT
 """
-import json
 import re
 import signal
 import sys
 
-# Regex pattern for input validation
-IP = "(25[0-5]|2[0-4]\d|1\d{2}|[0-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[0-9]?\d"
-DATE = "[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])"
-TIME = "(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\.\d{6}"
-STATUS = "200|301|400|401|403|404|405|500"
-SIZE = "\d{1,}"
-
-regex = f'^({IP}) \- (\[{DATE} {TIME}\]) ("GET /projects/260 HTTP/1.1") ({STATUS}) ({SIZE})$'
-
-# Metrics storage
+# Initialize counters and data storage
+total_file_size = 0
 stats = {
     200: 0,
     301: 0,
@@ -31,46 +23,68 @@ stats = {
     405: 0,
     500: 0
 }
-total_size = 0
 line_count = 0
 
+
+# Function to print accumulated metrics
 def print_stats(stats: dict) -> None:
-    """Prints the stats of a network session
+    """Prints the accumulated metrics
     Args:
-        stats(dict): Statistics of session.
+        stats(dict): Accumulated metrics.
             key - status code
             value - number of occurences of status code
     Return:
         None
     """
-    print(f"File size: {total_size}")
-    for code, no_of_occurence in stats.items():
+    print(f"File size: {total_file_size}")
+    for status_code in sorted(stats.keys()):
         # Only print status code that appeared
-        if no_of_occurence > 0:
-            print(f"{code}: {no_of_occurence}")
+        if stats[status_code] > 0:
+            print(f"{status_code}: {stats[status_code]}")
 
-# Interrupt signal handler
+
+# Signal handler for keyboard interruption (Ctrl + C)
 def interrupt_handler(signum, frame):
     """ Handles the SIGINT signal """
     print_stats(stats)
 
+
+# Register the signal handler
 signal.signal(signal.SIGINT, interrupt_handler)
 
 
-# Read from stdin line by line
+# Read lines from standard input
 for line in sys.stdin:
+    pattern = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)"
+
     # Update metrics if input data matches required format
-    match = re.search(regex, line)
-    if match:
-        line_count += 1  # Update line count
-        status_code = json.loads(match.group(11))  # Get status code
-        data_size = json.loads(match.group(12))  # Get size of data
+    try:
+        match = re.match(pattern, line)
+        if match:
+            ip_address, timestamp, status_code, file_size = match.groups()
+            total_file_size += int(file_size)
 
-        total_size += data_size
+            # Update metrics if status code is recognized
+            status_code = int(status_code)
+            if status_code in stats:
+                stats[status_code] += 1
 
-        if status_code:
-            stats[status_code] = stats.get(status_code) + 1
+            line_count += 1
 
-    # Display stats every 10 lines read
+    except (ValueError, ImportError):
+        pass  # Skip lines with incorrect file size or status code format
+
+    # Display stats after every 10 lines read
     if line_count % 10 == 0:
         print_stats(stats)
+
+# Strict regex pattern for input validation
+# FORMAT: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+# IP = r"(25[0-5]|2[0-4]\d|1\d{2}|[0-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[0-9]?\d"
+# DATE = r"[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])"
+# TIME = r"(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\.\d{6}"
+# STATUS = r"200|301|400|401|403|404|405|500"
+# SIZE = r"\d{1,}"
+
+# regex = rf'^({IP}) \- (\[{DATE} {TIME}\]) ("GET /projects/260 HTTP/1.1") ({STATUS}) ({SIZE})$'
